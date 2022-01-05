72d90bf7f859eb24073cf2e8030f55a053c6fa64
cst_dev162 <cst_dev162@80102093S-FER25>
2022-01-05 10:55:11 +0700

    learning-js

---
+++ mini-project-1/starter/app.js
@@ -0,0 +1,33 @@
+const projectTitle = document.querySelector("#project-title");
+
+projectTitle.textContent = "Title Baru dari JS File"
+
+//bikin variabel
+//camelCase = penamaan variabel huruf pertama kecil, kata selanjutnya besar
+var thisIsFunction = "this is function"
+const PI = 3.14
+
+//dalam js spasi tidak berpengaruh, titik koma tidak wajib juga. 
+var x = y + z;
+
+//bikin fungsi, code indentetion
+function sumNum(a, b) {
+  return a + b;
+}
+
+//casesensitive
+var iniSebuahVar = "var";
+iniSebuahVar = "asdad";
+//IniSebuahVar = "asdad"; > eror
+
+//bikinarray
+var array = ["bubur ayam", 
+  "kacang goreng", 
+  3, 
+  "bubur kacang ijo",
+  10,
+  2000,
+  "1999"
+];
+
+//namain file js kecil semua
